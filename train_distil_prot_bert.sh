4c04e7be38797caf19bc4789368844b87a6daaee
Łukasz Piekarski <wookashp98@gmail.com>
2023-06-29 09:54:59 +0200

    Add training script for distil prot bert

---
+++ train_distil_prot_bert.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=prot_simcse_lpiekarski-nlp
+#SBATCH --partition=nlp
+#SBATCH --qos=1gpu1d
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --output=output_distil_prot_bert.log
+#SBATCH --error=error_distil_prot_bert.log
+#SBATCH --time=0-3:00:00
+
+./venv/bin/python train.py \
+        --model_name_or_path yarongef/DistilProtBert \
+        --train_file data/uniprot_for_simcse.txt \
+        --output_dir trained_models/simcse_distil_prot_bert \
+        --num_train_epochs 1 \
+        --do_mlm \
+        --mlm_probability 0.15 \
+        --learning_rate 1e-5 \
+        --per_device_train_batch_size 64 \
+        --pooler_type cls \
+        --overwrite_output_dir \
+        --temp 0.05 \
+        --do_train \
+        --fp16 \
+        --gradient_accumulation_steps 8
